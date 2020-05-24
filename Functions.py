0d34ea6d669f8d0b75ddc03fba5181ba09a97c09
Zynox <jude.version1.0@gmail.com>
2020-05-24 22:39:53 +0100

    Push v1.2
    Huge update
    - Added support for any server or channel name
    - Added channel specific toggles, so on one channel it can be on, and on another it can be off
    125 lines -> 229 lines

--- Functions.py
+++ Functions.py
@@ -27,6 +27,7 @@ class functions():
 
     #Sets up and checks files for severs
     def create(client):
+        serverlist = client.guilds
         
         newpath = r"Servers"
         if os.path.exists(newpath):
@@ -35,30 +36,46 @@ class functions():
             os.makedirs(newpath)
             print(" > Created a folder to store server data called 'Servers'")
 
-        for server in client.guilds:
-            server = str(server)
-            if server == "IRREGULAR NAME":
-                server = "normal name"
-            newpath = r"Servers\{}".format(server)
+        try:
+            file = open("Servers\Server_Names.txt", "r")
+            file.close()
+            print(" > There is already a file to store server names\n")
+        except:
+            file = open("Servers\Server_Names.txt", "w+")
+            file.close()
+            print(" > Created a file to store server names called 'Server_Names.txt'\n")
+            
+        for server in serverlist:
+            serverid = str(server.id)
+            info = functions.checkserverid(serverid)
+            
+            if info[0] == serverid:
+                serverid = functions.getserverid(info[0], info[1])
+                print(" > {} was already in the datbase".format(server))
+            else:
+                functions.createserverid(serverid, info[1])
+                serverid = functions.getserverid(info[0], info[1])
+                print(" > Added {}'s to the database as 'Server {}'".format(server, serverid))  
+            
+            newpath = r"Servers\{}".format(serverid)
             if os.path.exists(newpath):
                 print(" > There is already a folder to store {}'s infomation".format(server))
             if not os.path.exists(newpath):
                 os.makedirs(newpath)
-                print(" > Created a folder to store {}'s infomation called '{}'".format(server, server))
-
+                print(" > Created a folder to store {}'s infomation called '{}'".format(server, serverid))            
+        
             try:
-                file = open("Servers\{}\Data.txt".format(server), "r")
+                file = open("Servers\{}\Data.txt".format(serverid), "r")
                 file.close()
+                print(" > There is already a file to store {}'s data\n".format(server))
                 
             except:
-                file = open("Servers\{}\Data.txt".format(server), "w+")
+                file = open("Servers\{}\Data.txt".format(serverid), "w+")
                 file.close()
+                print(" > Created a file to store {}'s data called 'Data.txt'\n".format(server))
 
     #Checks if there is a name for the gayscale feature
     def checkname(name, server):
-        server = str(server)
-        if server == "IRREGULAR NAME":
-            server = "normal name"
         file = open("Servers\{}\Data.txt".format(server), "r")
         data = file.readlines()
         file.close()
@@ -77,9 +94,6 @@ class functions():
 
     #Gets the sexuality of a member if their name was found
     def getsex(name, repeat, server):
-        server = str(server)
-        if server == "IRREGULAR NAME":
-            server = "normal name"
         file = open("Servers\{}\Data.txt".format(server), "r")
         data = file.readlines()
         repeat += 1
@@ -88,9 +102,6 @@ class functions():
 
     #Writes the sexuality of a member if their name isn't found
     def createsex(name, usersex, server):
-        server = str(server)
-        if server == "IRREGULAR NAME":
-            server = "normal name"
         file = open("Servers\{}\Data.txt".format(server), "r+")
         data = file.readlines()
         file.close()
@@ -107,19 +118,112 @@ class functions():
         file.close()
 
     #Creates a file for FBot's status
-    def setfbot(status, server):
-        server = str(server)
-        if server == "IRREGULAR NAME":
-            server = "normal name"
-        file = open("Servers\{}\Status.txt".format(server), "w+")
+    def setfbot(status, server, channel):
+        file = open("Servers\{}\{}\Status.txt".format(server, channel), "w+")
         file.writelines(status)
         file.close()
 
     #Gets FBot's status
-    def getfbot(server):
-        server = str(server)
-        if server == "IRREGULAR NAME":
-            server = "normal name"
-        file = open("Servers\{}\Status.txt".format(server), "r")
+    def getfbot(server, channel):
+        file = open("Servers\{}\{}\Status.txt".format(server, channel), "r")
         info = file.readlines()
         return info
+
+    #Checks for the Server ID
+    def checkserverid(name):
+        name = str(name)
+        file = open("Servers\Server_Names.txt", "r")
+        data = file.readlines()
+        file.close()
+        getname = ""
+        length = len(data)
+        length -= 1
+        repeat = -1
+        while repeat != length:
+            repeat += 2
+            getname = functions.remove(data[repeat], 14, 2)
+            getname = str(getname)
+            if name == getname:
+                break
+        info = [getname, repeat]
+        return info
+
+    #Gets the Server ID
+    def getserverid(name, repeat):
+        file = open("Servers\Server_Names.txt", "r")
+        data = file.readlines()
+        repeat -= 1
+        getid = functions.remove(data[repeat], 12, 2)
+        getid = "Server {}".format(getid)
+        return getid
+
+    #Creates a Server ID
+    def createserverid(name, serverid):
+        file = open("Servers\Server_Names.txt", "r+")
+        data = file.readlines()
+        file.close()
+        length = len(data)
+        serverid += 1
+        serverid /= 2
+        serverid += 1
+        serverid = int(serverid)
+        if length != 0:
+            length -= 1
+            lastrow = data[length]
+        else:
+            lastrow = ""
+            data = [""]
+        data[length] = "{}Server ID: '{}'\nServer Name: '{}'\n".format(lastrow, serverid, name)
+        file = open("Servers\Server_Names.txt", "w+")
+        file.writelines(data)
+        file.close()
+
+
+    #Checks for the Channel ID
+    def checkchannelid(name, serverid):
+        name = str(name)
+        file = open("Servers\{}\Channel_Names.txt".format(serverid), "r")
+        data = file.readlines()
+        file.close()
+        getname = ""
+        length = len(data)
+        length -= 1
+        repeat = -1
+        while repeat != length:
+            repeat += 2
+            getname = functions.remove(data[repeat], 15, 2)
+            getname = str(getname)
+            if name == getname:
+                break
+        info = [getname, repeat]
+        return info
+
+    #Gets the Channel ID
+    def getchannelid(name, repeat, serverid):
+        file = open("Servers\{}\Channel_Names.txt".format(serverid), "r")
+        data = file.readlines()
+        repeat -= 1
+        getid = functions.remove(data[repeat], 13, 2)
+        getid = "Channel {}".format(getid)
+        return getid
+
+    #Creates a Channel ID
+    def createchannelid(name, channelid, serverid):
+        file = open("Servers\{}\Channel_Names.txt".format(serverid), "r+")
+        data = file.readlines()
+        file.close()
+        length = len(data)
+        channelid += 1
+        channelid /= 2
+        channelid += 1
+        channelid = int(channelid)
+        if length != 0:
+            length -= 1
+            lastrow = data[length]
+        else:
+            lastrow = ""
+            data = [""]
+        data[length] = "{}Channel ID: '{}'\nChannel Name: '{}'\n".format(lastrow, serverid, name)
+        file = open("Servers\{}\Channel_Names.txt".format(serverid), "w+")
+        file.writelines(data)
+        file.close()
