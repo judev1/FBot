fce8e4cae847bf49c7c73536a968ef2418f0ac67
Zynox <jude.version1.0@gmail.com>
2020-08-15 02:47:20 +0100

    Commit v1.8.2.1
    Added an error message when no arguments are given

--- FBot_Cogs/Say.py
+++ FBot_Cogs/Say.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 class FBot_Cogs(commands.Cog):
@@ -7,14 +8,13 @@ class FBot_Cogs(commands.Cog):
         
     @commands.command(name="say")
     async def _Say(self, ctx, *message):
-        newmessage = ""
-        for words in message:
-            newmessage += words
-        if newmessage != "":
+        message = " ".join(message)
+        if len(message) == 0:
+            embed = discord.Embed(title="**Error:** `No Message`", description=f"```Usage: 'FBot say <message>'```", colour=0xF42F42)
+            await send(embed=embed)
+        else:
             await ctx.message.delete()
             await ctx.send(message)
 
 def setup(bot):
     bot.add_cog(FBot_Cogs(bot))
-
-        
