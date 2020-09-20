eaa7345e82c62acb677bca36efc54174e10e72f8
LinesGuy <gudgeon.christopher@gmail.com>
2020-09-20 19:24:54 +0100

    add Ppsize.py to initial_extensions

--- FBot.py
+++ FBot.py
@@ -39,7 +39,7 @@ async def on_ready():
         "Events", "FBotdev", "Infomation", "Join_Leave", "Links", "Modtoggle",
         "Notices", "PatchNotes", "Ping", "Prefix", "Priority", "Quote", "Say",
         "Session", "Status", "Trigger_Responses", "Version", "Snipe", "Bonk",
-        "Bigpp", "Joke", "Counter", "Purge"]
+        "Bigpp", "Joke", "Counter", "Purge", "Ppsize"]
     dev = ["FBotdev"]
 
     for extension in initial_extensions:
