cac648b8579754b73b343da97e6980f580842fbd
LinesGuy <gudgeon.christopher@gmail.com>
2020-09-22 17:24:38 +0100

    Add ban command

---
+++ FBot_Cogs/Ban.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class BanCog(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(name='ban')
+    @commands.is_owner()
+    async def ban(ctx, self, member: discord.Member):
+        await member.ban()
+        await ctx.send("Done")
+
+def setup(bot):
+    bot.add_cog(BanCog(bot))
