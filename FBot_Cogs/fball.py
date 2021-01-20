b42b325b11b7e9276dd9315ee1790b08e373a62c
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-01-20 14:30:32 +0000

    Add more answers to fball

--- FBot_Cogs/fball.py
+++ FBot_Cogs/fball.py
@@ -2,7 +2,14 @@ import discord
 from discord.ext import commands
 import random as random
 
-responses = ["yeah", "nah", "maybe", "idk"]
+responses = ["Without a doubt... yes.",
+             "Without a doubt... no.",
+             "Perhaps.",
+             "...Isn't it obvious?",
+             "You don't want to know.",
+             "HAHAHAHAHAHA no",
+             "I'd be lying if I said yes."
+             "yep"]
 
 class fball(commands.Cog):
 
