61c412b5ffb95ff0fea1af6558516b3fc18d5e4b
Zynox <jude.version1.0@gmail.com>
2021-01-20 16:08:16 +0000

    More fball

--- FBot_Cogs/fball.py
+++ FBot_Cogs/fball.py
@@ -4,12 +4,28 @@ import random as random
 
 responses = ["Without a doubt... yes.",
              "Without a doubt... no.",
+             "In my professional opinion: nah.",
+             "In my professional opinion: yup.",
+             "In my professional opinion: probs.",
+             "If I had to guess I'd say yes.",
+             "If I had to guess I'd say no.",
+             "If I had to guess I'd say maybe.",
+             "yep",
+             "NOT AT ALL! oh wait, actually yes",
+             "I'll say yes if you shut up",
+             "Most definitely not.",
+             "HAHAHAHAHAHA no.",
+             "I think so, mm, maybe not",
+             "I'd be lying if I said yes.",
              "Perhaps.",
+             "I'll think about it",
              "...Isn't it obvious?",
              "You don't want to know.",
-             "HAHAHAHAHAHA no",
-             "I'd be lying if I said yes."
-             "yep"]
+             "Are you for real?",
+             "Sensible questions only.",
+             "What are you asking me for?",
+             "Who knows! I don't!",
+             "m y  s e r v i c e s  a r e n ' t  f r e e  y e  k n o w"]
 
 class fball(commands.Cog):
 
