8c7e5fef203b272b43a3841f5bc6d910f0e17764
Jakub Kropáček <kropikuba@gmail.com>
2021-01-22 12:02:29 +0100

    Fixed bug

--- FBot_Cogs/economy.py
+++ FBot_Cogs/economy.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import MemberConverter
 import sqlite3
 import random
+from functions import fn
 
 conn = sqlite3.connect("economy.db")
 f = "~~f~~ "
@@ -34,15 +35,16 @@ class economy(commands.Cog):
     async def get_balance(self, ctx, mention: discord.Member=None):
         # Get a users balance
         # fbot bal, or fbot bal @mention
-        if (mention is None):
-            user_id = ctx.author.id
-        else:
+        if mention:
             user_id = mention.id
+        else:
+            user_id = ctx.author.id
         c = conn.cursor()
         t = (user_id,)
         c.execute("SELECT balance FROM users WHERE user_id=?", t)
         balance = c.fetchone()[0]
-        await ctx.send(f"Balance: {f}{balance}")
+        emb = fn.embed("Balance",f"{f}{balance}")
+        await ctx.send(embed=emb)
 
     @commands.command(name="devsetbalance", aliases=["devsetbal"])
     @commands.is_owner()
