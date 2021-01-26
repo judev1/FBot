9f49b07ef6782ba3b0316440f789117946fe33ad
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-01-26 16:09:04 +0000

    Avoid more than one purge per channel

--- FBot_Cogs/purge.py
+++ FBot_Cogs/purge.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import asyncio
 
+ongoing_purges = set()
+
 class purge(commands.Cog):
 
     def __init__(self, bot):
@@ -10,7 +12,13 @@ class purge(commands.Cog):
     @commands.guild_only()
     @commands.bot_has_permissions(manage_messages=True)
     async def do_purge(self, ctx, *args):
-
+        if ctx.channel.id in ongoing_purges:
+            msg = await ctx.send("**A purge is already in progress, calm yoself.**")
+            await asyncio.sleep(1)
+            try:
+                await msg.delete()
+            except: pass # message might have been deleted
+            return
         if len(args) != 0:
             limit = "".join(args)
             if limit.isdigit():
@@ -18,7 +26,9 @@ class purge(commands.Cog):
                 if limit > 1000:
                     await ctx.send("`The purge limit is currently 1000 messages`")
                     return
+                ongoing_purges.add(ctx.channel.id)
                 await ctx.channel.purge(limit=limit + 1)
+                ongoing_purges.remove(ctx.channel.id)
                 msg = await ctx.send(f"`Purged {limit} messages.`")
                 await asyncio.sleep(1)
                 await msg.delete()
