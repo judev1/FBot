6bbad632f26938677c833ce3972d006bf86f1bd7
Jude <jude.cowden@protonmail.com>
2021-06-09 21:31:56 +0100

    Added buttons to snake

--- cogs/snake.py
+++ cogs/snake.py
@@ -1,10 +1,15 @@
 from discord.ext import commands
 from collections import deque
 from random import randint, choice
+from discord_components import Button, ButtonStyle, InteractionType
 import asyncio
 
-emojis = ["⬆️", "⬇️", "⬅️", "➡️"]
-emojinames = ["up", "down", "left", "right"]
+emojis = {
+    "up": "⬆️",
+    "down": "⬇️",
+    "left": "⬅️",
+    "right": "➡️"
+}
 
 class snakegame():
 
@@ -57,6 +62,11 @@ class snakegame():
         board += ":white_large_square:" * (self.width + 2)
         return board
 
+    def won(self):
+        if self.width * self.height == len(self.snake):
+            return True
+        return False
+
     def move(self):
         x, y = self.snake[0]
         direction = self.direction
@@ -87,24 +97,40 @@ class snakegame():
         while (x, y) in self.snake:
             x, y = randint(0, self.width-1), randint(0, self.height-1)
         return (x, y)
-    
 
 class snake(commands.Cog):
-    
+
     def __init__(self, bot):
         self.bot = bot
         self.games = {}
-        
-    @commands.command(name="snake", alliases=["snek"])
-    async def _Snake(self, ctx):
-        
-        def speed():
-            return 0.8 #round(0.8 - (game.score/100)**0.5, 3) #round(0.8 - (game.score**2) / 5120, 3)
 
-        def snake_embed():
+    @commands.command(name="snake", aliases=["snek"])
+    async def _Snake(self, ctx):
+
+        def embed():
             embed = self.bot.fn.embed(ctx.author, "Snake Game", game.board())
-            embed.set_author(name=f"{game.score} points | {game.direction.upper()}") # {str(speed())[1:]}fps
+            embed.set_author(name=f"{game.score} points | {game.direction.upper()}")
             return embed
+
+        def components(ignore):
+
+            x = ["up", "down"]
+            y = ["left", "right"]
+
+            def button(direction):
+                disabled = False
+                if ignore in x and direction in x:
+                    disabled = True
+                elif ignore in y and direction in y:
+                    disabled = True
+                return Button(style=ButtonStyle.blue, emoji=emojis[direction], disabled=disabled)
+
+            null = Button(style=ButtonStyle.grey, label="-", disabled=True)
+            row_one = [null, button("up"), null]
+            row_two = [button("left"), null, button("right")]
+            row_three = [null, button("down"), null]
+
+            return [row_one, row_two, row_three]
 
         user_id = ctx.author.id
         if user_id in self.games:
@@ -112,27 +138,42 @@ class snake(commands.Cog):
             return
 
         game = self.games[user_id] = snakegame()
-        msg = await ctx.send(embed=snake_embed())
-
-        for emoji in emojis:
-            await msg.add_reaction(emoji)
+        msg = await ctx.send(embed=embed(), components=components("right"))
+        game.message_id = msg.id
 
         while game.alive:
-            await asyncio.sleep(speed())
+            await asyncio.sleep(0.8)
             game.move()
-            try: await msg.edit(embed=snake_embed())
+            try: await msg.edit(embed=embed(), components=components(game.direction))
             except: break
+
+        buttons = []
+        if game.won():
+            button = Button(style=ButtonStyle.green, label="You won!", disabled=True)
+        else:
+            button = Button(style=ButtonStyle.red, label="You died!", disabled=True)
+        buttons.append(button)
+
+        button = Button(style=ButtonStyle.green, label=f"Score: {game.score}", disabled=True)
+        buttons.append(button)
+
+        try: await msg.edit(embed=embed(), components=[buttons])
+        except: pass
+
         del self.games[user_id]
 
-        await ctx.send(f"**You died with a score of {game.score}!**")
-
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        if user.id not in self.games: return
-        emoji = reaction.emoji
-        if emoji in emojis:
-            self.games[user.id].direction = emojinames[emojis.index(emoji)]        
-        await reaction.message.remove_reaction(reaction, user)
-        
+    async def on_button_click(self, res):
+        user_id = res.author.id
+        if user_id in self.games:
+            if res.message.id == self.games[user_id].message_id:
+                if hasattr(res.component, "emoji"):
+                    emoji = res.component.emoji.name
+                    for direction in emojis:
+                        if emoji == emojis[direction]:
+                            self.games[user_id].direction = direction
+                            break
+        await res.respond(type=InteractionType.DeferredUpdateMessage)
+
 def setup(bot):
     bot.add_cog(snake(bot))
