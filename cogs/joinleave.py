1dbe1ca9b198e7270ae892eed75960a918c14021
Jude <jude.cowden@protonmail.com>
2021-07-18 17:07:01 +0100

    Updated join message

--- cogs/joinleave.py
+++ cogs/joinleave.py
@@ -1,21 +1,23 @@
 from discord.ext import commands
-import lib.functions as fn
 import lib.database as db
 
-joinmsg = ["**Whoa new server, cooooool**",
+joinmsg = ["**Whoa new server, cooooool!**",
            "Thanks for choosing FBot, here are some helpful tips and tricks to get you started",
 
-           "", ":speech_balloon: **How do I turn on FBot's spamming/message response feature?**",
-           "You can turn this on by using the command `fbot on` (it's case insensitive) in the desired channel and FBot will begin replying to your messages. If only want admins to be able to toggle FBot, you can use `fbot modtoggle on` to do this",
+           ":face_with_symbols_over_mouth: **How do I turn on FBot's spamming/message response feature?**",
+           "You can turn this on by using the command `fbot on` (it's case insensitive) in the desired channel and FBot will begin __replying__ to your messages. If only want admins to be able to toggle FBot, you can use `fbot modtoggle on`",
 
-           "", ":1234: **How can I set up a counting channel with FBot?**",
-           "In the channel which you want to be your counting channel, use `fbot counting` and simply start counting! You can count on it! Try get the highest number (you can check your status by using `fbot top counting`) and you'll be successfully after completeing kindergarten!",
+           ":thinking: **How can I set up a counting channel with FBot?**",
+           "In the desired channel, use `fbot set` and simply start counting! The rules are simple: send the next number, and don't send two numbers in a row. You can check your highscore, the current number and more with `fbot counting`",
 
-           "", ":sound: **I'm seeing too much FBot how can I calm it down?**",
+           ":face_with_spiral_eyes: **I'm seeing *way too much* FBot, how can I chill it a bit?**",
            "If you want FBot to respond to less messages you can use `fbot respond few`, by default fbot respond is set to `all`, but you can change it to few, some or all, depending on what suits your server",
 
-           "", ":safety_pin: **Anything else?**",
-           "Well if you need anything else use `fbot help` & `fbot cmds`, where you can find a list of commands, and if you are after finding a bug, have a suggestion or just a general question about FBot, use `fbot server` to get a *fresh* invite to our support server!"]
+           ":zany_face: **That's great and all but are there any other fun features?**",
+           "So. Fricken. Many. Image comamnds like `fbot god`, say commands like `fbot biblical`; where you can provide text or reply to a message, same goes for `fbot respects` which is used to pay respects, we've got minigames like `fbot snake`. And so, so much more!",
+
+           ":sleeping: **Sounds great! There can't be anything else, can there?**",
+           "Well yes, there is but you'll have to find it out for yourself, with `fbot help` & `fbot cmds`, which will give you a list of commands! And if you find a bug, have a suggestion or just wanna chat, use `fbot server` to enter ~~hell~~ our server!"]
 
 class fakeuser: id = 0
 user = fakeuser()
@@ -25,21 +27,21 @@ class joinleave(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.serverlogs = self.bot.get_channel(720923733132312587)
+        self.joinmsg = joinmsg
 
     @commands.Cog.listener()
     async def on_guild_join(self, newguild):
         db.addguild(newguild.id)
 
-        try:
-            try:
+        system_channel = newguild.system_channel
+        if system_channel:
+            bot_perms = system_channel.permissions_for(newguild.get_member(self.bot.user.id))
+
+            if bot_perms.send_messages:
                 embed = self.bot.embed(user, joinmsg[0], joinmsg[1])
-                for i in range(3, 16, 3):
-                    embed.add_field(name=joinmsg[i], value=joinmsg[i+1],
-                                    inline=False)
-                await newguild.system_channel.send(embed=embed)
-            except:
-                await newguild.system_channel.send("\n".join(joinmsg))
-        except: pass
+                for i in range(2, 11, 2):
+                    embed.add_field(name=joinmsg[i], value=joinmsg[i+1], inline=False)
+                await system_channel.send(embed=embed)
 
         memcount = newguild.member_count
         embed = self.bot.embed(user, f"**Added** to `{newguild}`",
