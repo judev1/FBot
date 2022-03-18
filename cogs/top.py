d9ce6c9c40cdb1a14f35943542a2185adbb571c4
Jude <jude.cowden@protonmail.com>
2022-03-18 17:16:55 +0000

    top counters in dms

--- cogs/top.py
+++ cogs/top.py
@@ -14,7 +14,13 @@ toptypes = {
 }
 
 medals = {1: ":first_place:", 2: ":second_place:", 3: ":third_place:"}
-prefixes = {1: "st", 2: "nd", 3: "rd"}
+suffixes = {1: "st", 2: "nd", 3: "rd"}
+
+def suffix(rank):
+    if int(str(rank)[-1]) in [1, 2, 3] and rank not in [11, 12, 13]:
+        return f"{rank}{suffixes[rank]}"
+    else:
+        rank = f"{rank}th"
 
 NAME = 0
 USAGE = 1
@@ -35,23 +41,23 @@ class Top(commands.Cog):
             obj_id = ctx.author.id
 
             if data[USAGE] == "server":
-                obj_id = ctx.guild.id
-                if str(ctx.channel.type) == "private":
-                    await ctx.reply(f"Top {data[NAME]} can only be used in a server")
-                    return
+                obj_id = -1
+                if str(ctx.channel.type) != "private":
+                    obj_id = ctx.guild.id
 
             async with ctx.channel.typing():
-                top, selftop, rank = db.gettop(data[NAME], 12, obj_id)
+                top, score, rank = db.gettop(data[NAME], 12, obj_id)
                 embed = self.bot.embed(
                     ctx.author,
                     f"FBot Top {data[NAME]}",
-                    f"Ranked `{rank}` " + data[TOP].format(selftop)
                 )
+                if obj_id != -1:
+                    embed.description = f"Ranked `{suffix(rank)}` {data[TOP].format(score)}"
 
                 cache = self.bot.cache.names
                 for rank, row in enumerate(top):
 
-                    ID, total = row
+                    ID, score = row
                     name = cache.get(ID)
 
                     if not name:
@@ -75,13 +81,10 @@ class Top(commands.Cog):
                     if rank in medals:
                         medal = medals[rank]
 
-                    if int(str(rank)[-1]) in [1, 2, 3] and rank not in [11, 12, 13]:
-                        rank = f"{medal} {rank}{prefixes[rank]}"
-                    else:
-                        rank = f":medal: {rank}th"
+                    rank = f"{medal} {suffix(rank)}"
 
                     embed.add_field(
-                        name=f"{rank} {data[RANK].format(total)}",
+                        name=f"{rank} {data[RANK].format(score)}",
                         value=name
                     )
 
